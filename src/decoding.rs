21c912c3990186072112f96c500de0a3a2997eb5
Sebastian Beyvers <sb@pus.de>
2024-11-05 15:41:20 +0100

    feat: Implement partial eq for decoding key

--- src/decoding.rs
+++ src/decoding.rs
@@ -42,7 +42,7 @@ macro_rules! expect_two {
     }};
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
 pub(crate) enum DecodingKeyKind {
     SecretOrDer(Vec<u8>),
     RsaModulusExponent { n: Vec<u8>, e: Vec<u8> },
@@ -50,7 +50,7 @@ pub(crate) enum DecodingKeyKind {
 
 /// All the different kind of keys we can use to decode a JWT.
 /// This key can be re-used so make sure you only initialize it once if you can for better performance.
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
 pub struct DecodingKey {
     pub(crate) family: AlgorithmFamily,
     pub(crate) kind: DecodingKeyKind,
